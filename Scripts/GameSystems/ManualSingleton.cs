09aa3e7748ac1195f40914319e02af45dbfda76d
anton.grebennikov <anton.grebennikov@matryoshkagames.com>
2019-11-21 01:44:29 +0700

    ManualSingletor - вариант MonoSingleton без автосоздания в случае отсутствия

---
+++ Scripts/GameSystems/ManualSingleton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace SMGCore {
+	public class ManualSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+		public static T Instance {
+			get {
+				if ( !_instance ) {
+					_instance = (T)FindObjectOfType(typeof(T));
+				}
+				return _instance;
+			}
+		}
+
+		static T _instance = null;
+
+		protected virtual void Awake() {
+			if ( _instance != null && _instance != this ) {
+				Destroy(this);
+				return;
+			}
+
+			if ( _instance == null ) {
+				_instance = gameObject.GetComponent<T>();
+			}
+		}
+	}
+}
