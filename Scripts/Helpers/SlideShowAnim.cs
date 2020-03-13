9baa26c1b4607f2c7c7caf2d94e4fee59bcdfc86
anton.grebennikov <anton.grebennikov@matryoshka.com>
2020-03-14 01:32:29 +0700

    Отлавливаем паузу в анимации слайдшоу

--- Scripts/Helpers/SlideShowAnim.cs
+++ Scripts/Helpers/SlideShowAnim.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using SMGCore.EventSys;
+
 namespace SMGCore {
 	public sealed class SlideShowAnim : MonoBehaviour {
-		public List<Sprite> Sprites = new List<Sprite>();
-		public List<float>  Delays  = new List<float>();
-		public bool         Loop    = true;
+		public List<Sprite> Sprites    = new List<Sprite>();
+		public List<float>  Delays     = new List<float>();
+		public bool         Loop       = true;
+		public bool         WatchPause = true;
 
 		float          _time          = 0f;
 		float          _nextFrameTime = 0f;
 		int            _frameIndex    = 0;
 		SpriteRenderer _renderer      = null;
 		Image          _image         = null;
+		bool           _isPause       = false;
 
 		void Start() {
+			EventManager.Subscribe<Event_PauseChange>(this, OnPauseChange);
 			_renderer      = GetComponent<SpriteRenderer>();
 			_image         = GetComponent<Image>();
 			_nextFrameTime = Time.time + Delays[_frameIndex];
@@ -34,6 +39,9 @@ namespace SMGCore {
 		}
 
 		void Update() {
+			if ( _isPause && WatchPause ) {
+				return;
+			}
 			_time += Time.deltaTime;
 			if ( Time.time > _nextFrameTime ) {
 				if ( _renderer ) {
@@ -46,11 +54,18 @@ namespace SMGCore {
 				_frameIndex++;
 				if ( _frameIndex >= Sprites.Count && Loop ) {
 					_frameIndex = 0;
-				}
-				else if ( _frameIndex >= Sprites.Count ) {
+				} else if ( _frameIndex >= Sprites.Count ) {
 					_frameIndex = Sprites.Count - 1;
 				}
 			}
+		}
+
+		void OnDestroy() {
+			EventManager.Unsubscribe<Event_PauseChange>(OnPauseChange);
+		}
+
+		void OnPauseChange(Event_PauseChange e) {
+			_isPause = e.Pause;
 		}
 	}
 }
