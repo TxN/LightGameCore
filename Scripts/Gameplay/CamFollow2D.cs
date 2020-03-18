34beaf5dcc40c71b88e20efa44dec65650a34696
anton.grebennikov <anton.grebennikov@matryoshka.com>
2020-03-19 00:04:44 +0700

    Доработал следящую камеру

--- Scripts/Gameplay/CamFollow2D.cs
+++ Scripts/Gameplay/CamFollow2D.cs
@@ -2,26 +2,48 @@ using UnityEngine;
 
 namespace SMGCore {
 	public sealed class CamFollow2D : MonoSingleton<CamFollow2D> {
-		public AnimationCurve lerpCoef  = null;
-		public Transform      player    = null;
-		public float          initDelta = 10f;
+		public AnimationCurve lerpCoef           = null;
+		public Transform      player             = null;
+		public float          initDelta          = 10f;
+		public Transform      DebugGizmo         = null;
+		public Vector3        ShiftOffset        = new Vector3(0, 0, 0);
+		public AnimationCurve VerticalShiftCurve = null;
+		public bool           VerticalSpeedShift = false;
 
 		float _initZ     = 0f;
 		float _moveError = 0f;
 
+		Vector3 _targetLastPos    = Vector3.zero;
+
 		void Start() {
+			if ( VerticalShiftCurve != null ) {
+				VerticalShiftCurve.preWrapMode = WrapMode.Clamp;
+				VerticalShiftCurve.postWrapMode = WrapMode.Clamp;
+			}
 			_initZ = transform.position.z;
+			_targetLastPos = player.position;
 		}
 
 		void LateUpdate() {
 			if ( !player ) {
 				return;
 			}
-			_moveError = Vector3.Distance(transform.position, player.position) - initDelta;
+			var targetPosition = player.position;
+			targetPosition += ShiftOffset;
+			var vSpeed = (player.position.y - _targetLastPos.y) / Time.deltaTime;
+			if ( VerticalSpeedShift && vSpeed < 0 ) {
+				var val = VerticalShiftCurve.Evaluate(-vSpeed);
+				targetPosition += Vector3.up * val;
+			}
+			_moveError = Vector3.Distance(transform.position, targetPosition) - initDelta;
 			var cLerp = lerpCoef.Evaluate(_moveError);
-			var newPos = Vector3.Lerp(transform.position, player.position, cLerp);
+			var newPos = Vector3.Lerp(transform.position, targetPosition, cLerp * Time.deltaTime);
 			newPos.z = _initZ;
 			transform.position = newPos;
+			if ( DebugGizmo ) {
+				DebugGizmo.transform.position =  new Vector3(targetPosition.x, targetPosition.y, 0.25f);
+			}
+			_targetLastPos = player.position;
 		}
 
 		public float Map(float value, float fromSource, float toSource, float fromTarget, float toTarget, bool clamp = false) {
