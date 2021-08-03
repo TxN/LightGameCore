67f0191c5158b7b111e0be987811c9ca1718fc18
Anton Grebennikov <anton.grebennikov@matryoshka.com>
2021-08-03 16:53:28 +0700

    Доработка ManualSingleton: больше варнингов, иной способ получения инстанса синглтона.

--- Scripts/GameSystems/ManualSingleton.cs
+++ Scripts/GameSystems/ManualSingleton.cs
@@ -5,7 +5,10 @@ namespace SMGCore {
 		public static T Instance {
 			get {
 				if ( !_instance ) {
-					_instance = (T)FindObjectOfType(typeof(T));
+					_instance = FindObjectOfType<T>(true);
+					if ( !_instance ) {
+						Debug.LogWarning($"ManualSingleton({typeof(T).ToString()}).InstanceProperty: Cannot find instance");
+					}
 				}
 				return _instance;
 			}
@@ -15,12 +18,16 @@ namespace SMGCore {
 
 		protected virtual void Awake() {
 			if ( _instance && _instance != this ) {
+				Debug.LogWarning($"ManualSingleton({typeof(T).ToString()}).Awake: another alive instance is found");
 				Destroy(this);
 				return;
 			}
 
 			if ( !_instance ) {
-				_instance = gameObject.GetComponent<T>();
+				_instance = GetComponent<T>();
+				if ( !_instance ) {
+					Debug.LogWarning($"ManualSingleton({typeof(T).ToString()}).Awake: Unable to set object as singleton instance: cannot find such component.");
+				}
 			}
 		}
 	}
