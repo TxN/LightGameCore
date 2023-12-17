dbf029cc770bf7ce128c711ca2d2d061cfd1bfe3
Anton Grebennikov <anton.grebennikov@matryoshka.com>
2023-12-17 22:32:21 +0100

    Простенький утильскрипт для постоянного вращения объекта вокруг оси

---
+++ Scripts/Helpers/ConstantRotation.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace SMGCore {
+	public class ConstantRotation : MonoBehaviour {
+		public Vector3 LocalAxis;
+		public float Speed = 30f;
+
+		void Update() {
+			transform.Rotate(LocalAxis, Speed * Time.deltaTime);
+		}
+	}
+}
