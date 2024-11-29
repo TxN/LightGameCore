55dd7099013eaad73e3a5bebdf5ea5cdff1bb4b3
anton.grebennikov <anton.grebennikov@matryoshka.com>
2024-11-30 01:52:26 +0700

    Возможность временно подавить эффект тряски камеры

--- Scripts/Gameplay/CameraShake.cs
+++ Scripts/Gameplay/CameraShake.cs
@@ -5,6 +5,7 @@ namespace SMGCore {
 	public sealed class CameraShake : MonoBehaviour {
 		public float MaxAmount = 5f;
 		public float MaxDuration = 2f;
+		public bool IsSuppressed = false; //подавляем тряску, но продолжаем "проигрывать" эффект
 		public bool debugMode = false; //Calls ShakeCamera() on start
 
 		public float shakeAmount;  //The amount to shake this frame.
@@ -62,6 +63,9 @@ namespace SMGCore {
 			isRunning = true;
 
 			while ( shakeDuration > 0.01f ) {
+				if ( Time.timeScale < 0.01f ) {
+					yield return null;
+				}
 				if ( decay ) {
 					shakeDuration = Mathf.Clamp(shakeDuration - Time.deltaTime, 0, shakeDuration);
 				}
@@ -73,12 +77,12 @@ namespace SMGCore {
 				shakeAmount = startAmount * shakePercentage;//Set the amount of shake (% * startAmount).
 															//   shakeDuration = Mathf.Lerp(shakeDuration, 0, Time.deltaTime);//Lerp the time, so it is less and tapers off towards the end.
 
-
-				if ( smooth )
-					transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(rotationAmount), Time.deltaTime * smoothAmount);
-				else
-					transform.localRotation = Quaternion.Euler(rotationAmount);//Set the local rotation the be the rotation amount.
-
+				if ( !IsSuppressed ) {
+					if ( smooth )
+						transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(rotationAmount), Time.deltaTime * smoothAmount);
+					else
+						transform.localRotation = Quaternion.Euler(rotationAmount);//Set the local rotation the be the rotation amount.
+				}
 				yield return null;
 			}
 			transform.localRotation = Quaternion.identity;//Set the local rotation to 0 when done, just to get rid of any fudging stuff.
